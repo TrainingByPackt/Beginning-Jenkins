f72c88ac609e307f6a4ee71438ec4337c9df93e7
arnoldokoth <arnold.okoth@andela.com>
2018-05-05 18:09:53 +0300

    Update Vagrant provisioning scripts

--- Lesson 6-Distributed Builds on Jenkins/provision_master.sh
+++ Lesson 6-Distributed Builds on Jenkins/provision_master.sh
@@ -1,13 +1,11 @@
 # Install Java
-sudo yum update -y
-sudo yum remove java -y
 sudo yum install java-1.8.0-openjdk -y
 java -version
 
 # Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum install jenkins -y
 
 # Start Jenkins & ensure Jenkins is started at startup
 sudo service jenkins start
--- Lesson 6-Distributed Builds on Jenkins/provision_slave.sh
+++ Lesson 6-Distributed Builds on Jenkins/provision_slave.sh
@@ -1,5 +1,3 @@
 # Install Java
-sudo yum update -y
-sudo yum remove java -y
 sudo yum install java-1.8.0-openjdk -y
 java -version
